0d9e2ab1e9f5fe5ffcc3188f27c8a046d2b2c244
abdulfalll <59912088+abdulfalll@users.noreply.github.com>
2026-05-05 12:48:00 +0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@ function divi_child_theme_enqueue_assets() {
     // 1. Enqueue Parent Theme Style
     wp_enqueue_style( 'divi-parent-style', get_template_directory_uri() . '/style.css' );
 
-    // 2. Enqueue Custom CSS (from assets folder)
-    wp_enqueue_style( 'divi-child-custom-style', get_stylesheet_directory_uri() . '/assets/css/custom-styles.css', array('divi-parent-style'), '1.0.0' );
+// 2. Enqueue Custom CSS (from the root style.css)
+    wp_enqueue_style( 'divi-child-custom-style', get_stylesheet_directory_uri() . '/style.css', array('divi-parent-style'), '1.0.1' );
 
     // 3. Enqueue Custom JavaScript (from assets folder)
     wp_enqueue_script( 'divi-child-custom-js', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery'), '1.0.0', true );
